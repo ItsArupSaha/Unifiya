46613730a4bc3c73282f7e3191cf9d2f5270c973
Arup Saha <sahaarup911@gmail.com>
2024-12-02 15:52:06 +0600

    meeting setup is completed with camera and mic  permission

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,20 +7,24 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
-const Meeting = ({ params: {id} }: { params: { id: string } }) => {
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
 
-  const [isSetupComplete, setisSetupComplete] = useState(false);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  const {call, isCallLoading} = useGetCallById(id);
+  const { call, isCallLoading } = useGetCallById(id);
 
-  if(!isLoaded || isCallLoading) return <Loader />
+  if (!isLoaded || isCallLoading) return <Loader />;
 
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete ={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
